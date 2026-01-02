dba2c488b73d3587000fa1db8db30dd4b882424a
Geoff Krishnan <geoffrykrishnan@gmail.com>
2026-01-01 18:00:50 -0600

    feat: implement markdown to blocks function
    convert markdown string to list of block strings

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -166,9 +166,10 @@ def text_to_textnodes(text):
     return text_nodes
 
 
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    main()
+def markdown_to_blocks(markdown):
+    # regex substitutes \n with one or more spaces after it with \n effectively removing the tabs created after newlines
+    return [
+        re.sub(r"\n\s+", "\n", block.strip())
+        for block in markdown.split("\n\n")
+        if block.strip()
+    ]
